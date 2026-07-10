820391ec52279d5df15008cb522a37ad22f7d75e
Jeremy Edwards <1312331+jeremyje@users.noreply.github.com>
2026-07-10 08:05:05 +0000

    Minor fixes

--- Makefile_build.mk
+++ Makefile_build.mk
@@ -89,7 +89,7 @@ build/certs/codesign.crt build/certs/codesign.key &: $(CERTTOOL)
 endif
 
 build/bin/%: $(ASSETS)
-	GOOS=$(word 3, $(subst /, ,$(dir $@))) GOARCH=$(word 4, $(subst /, ,$(dir $@))) GOARM=$(subst v,,$(word 5, $(subst /, ,$(dir $@)))) CGO_ENABLED=0 $(GO) build -ldflags="-X 'github.com/cloudfra/template-go/internal.version=$(VERSION)' -X 'github.com/cloudfra/template-go/internal.buildstamp=$(BUILD_DATE)'" -o $@ cmd/$(basename $(notdir $@))/$(basename $(notdir $@)).go
+	GOOS=$(word 3, $(subst /, ,$(dir $@))) GOARCH=$(word 4, $(subst /, ,$(dir $@))) GOARM=$(subst v,,$(word 5, $(subst /, ,$(dir $@)))) CGO_ENABLED=0 $(GO) build -ldflags="-X '$(GO_PACKAGE)/internal.version=$(VERSION)' -X '$(GO_PACKAGE)/internal.buildstamp=$(BUILD_DATE)'" -o $@ cmd/$(basename $(notdir $@))/$(basename $(notdir $@)).go
 	touch $@
 
 lint: lint-go lint-terraform lint-docker lint-yaml lint-shell lint-vuln

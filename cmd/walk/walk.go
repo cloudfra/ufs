e1bde07eca23d98712197f738ca88fe698bf8486
Jeremy Edwards <1312331+jeremyje@users.noreply.github.com>
2026-07-09 17:40:31 -0700

    Add package comment for walk application
    Added package main comment to clarify the purpose of the walk application.

--- cmd/walk/walk.go
+++ cmd/walk/walk.go
@@ -12,7 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package ufs
+// Package main is the walk application for traversing through a virtual file system.
+package main
 
 import (
 	"context"
